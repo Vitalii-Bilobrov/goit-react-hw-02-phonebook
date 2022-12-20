3d23a91e44c04d33c4a7f01bcebf51e28cb24e00
Vitalii-Bilobrov <106316488+Vitalii-Bilobrov@users.noreply.github.com>
2022-12-20 14:31:58 +0200

    20

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Form } from './Form/Form';
+import { Contacts } from './Contacts/Contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.formSubmitHandler} />
+        <Contacts />
+      </>
+    );
+  }
+}
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { nanoid } from 'nanoid';
+
+export const Contacts = () => (
+  <>
+    <h1>Contacts</h1>
+    <ul>
+      <li></li>
+    </ul>
+  </>
+);
